b8eab9945192f2033f8eb92bb897d016382adbed
JAYARAJ NARAYANAN <jayarajcs2011@hotmail.com>
2024-04-27 01:14:45 +0530

    SecondCommit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hellow World");
+console.log("Hellow World")
+const myArray= new Array("One","Two","Three")
+let  result=myArray.toString();
+console.log(result);
+console.log(typeof(myArray))
+console.log(typeof(result))
+console.log(Array.isArray(myArray))
+console.log(Array.isArray(result))
+console.log(myArray instanceof Array)
+myArray[8]="Seven"
+console.log(myArray)
+console.log(Object.keys(myArray))
+console.log(myArray.length)
+myArray.length=20;
+console.log(myArray.length)
+console.log(myArray[3])
